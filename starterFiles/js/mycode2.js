2f8df417558953f78f97c326370dd0d296bdc523
d3nise <denise.thuyvy@gmail.com>
2018-06-02 20:58:42 -0700

    Student files blank temp and outlined funtions

---
+++ starterFiles/js/mycode2.js
@@ -0,0 +1,116 @@
+ /*
+    #######################################################
+    FILENAME: mycode.js
+    OVERVIEW: GDI JS Class 2
+	PURPOSE: Different Let's Develop It 
+ 			Blank temp for students
+    #######################################################
+*/
+
+/*	
+	######################################################
+	To declare (create) a function, you can give it a name, 
+	then include all the code for the function inside curly 
+	brackets {}
+	#######################################################
+*/
+function parrotFacts() {
+	console.log('Some parrot species can live for over 80 years');
+	console.log('Kakapos are a critically endangered flightless parrot');
+}
+  
+/*
+	######################################################
+	Let’s Develop It 2.1
+	Write a function that outputs a sentence. 
+	Then invoke that function later in your code.
+	Print it out to your HTML page using 
+	document.getElementById("TARGET").innerHTML=(variable 
+	or text);
+	######################################################
+*/
+function sentence(){
+
+}
+
+/*
+	######################################################
+	Let’s Develop It 2.2
+	Write a simple program to combine a first name and a 
+	last name inside a function. 
+	Then update the function to accept a first and last 
+	name as arguments.
+	######################################################
+*/
+//Prints to HTML page when function is called
+function nameFunction1(){
+
+}
+
+//Prints into text field when clicked
+function nameFunction2(){
+
+}
+
+//Changes texts when onclicked
+function nameFunction3(){
+
+}
+
+//Return statement
+function square(num) {
+	return num * num;
+}
+console.log(square(4));       // outputs '16'
+
+
+/*
+	######################################################
+	Let’s Develop It 2.3
+	Add a return statement to your 'name' function. 
+	Use that function to set the value of a variable.
+	######################################################
+*/
+//Returns statment for name to consol log 
+function nameStatement(first,last) {
+	
+}
+
+
+/*	
+	######################################################
+	Let’s Develop It 2.4
+	Make a variable called "temperature". Write some code 
+	that tells 	you to put on a coat if it is below 50 
+	degrees.
+	######################################################
+*/
+function temp1(){
+
+}
+
+/*
+	######################################################	
+	Let’s Develop It 2.5
+	Modify your "wear a coat" code for these conditions:
+	Implement with user input and onclick.
+	CONDITIONS
+	If it's less than 50 degrees, wear a coat.
+	If it's less than 30 degrees, wear a coat and a hat.
+	If it's less than 0 degrees, stay inside.
+	Otherwise, wear whatever you want.
+	######################################################
+*/
+function temp2(){
+
+}
+
+/*
+	######################################################
+	Let’s Develop It 2.6
+	Add a logical operator to your "what to wear" program.
+	######################################################
+*/
+function temp3(){
+
+}
